a1141601a73863183a4df1ee8783b0b956dcb035
Bernhard Huber <bernhard.huber2@liebherr.com>
2023-04-11 14:50:01 +0200

    created LiStreamContextFactory

---
+++ LiStreamEF/LiStreamContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using LiStreamEF.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace LiStreamEF
+{
+    public class LiStreamContextFactory : IDesignTimeDbContextFactory<LiStreamContext>
+    {
+        public LiStreamContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<LiStreamContext>();
+        #pragma warning disable CS8604 // Possible null reference argument.
+            optionsBuilder.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+        #pragma warning restore CS8604 // Possible null reference argument.
+
+
+            return new LiStreamContext(optionsBuilder.Options);
+        }
+    }
+}
